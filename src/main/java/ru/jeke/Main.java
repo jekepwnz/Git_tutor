87a7439df2b2e0548ecc2b75429448c80c2b5b7f
Evgeniy Volkov <hardcoreokje@gmail.com>
2025-02-02 00:15:10 +0300

    changed message

--- src/main/java/ru/jeke/Main.java
+++ src/main/java/ru/jeke/Main.java
@@ -2,6 +2,6 @@ package ru.jeke;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, my first branch is working!");
+        System.out.println("Hello, i changed this!");
     }
 }
