d5e260a618452d3d0f007f5c839dfa0177e9a093
jekepwnz <108674938+jekepwnz@users.noreply.github.com>
2025-02-02 00:20:56 +0300

    Update Main.java

--- src/main/java/ru/jeke/Main.java
+++ src/main/java/ru/jeke/Main.java
@@ -3,7 +3,7 @@ package ru.jeke;
 public class Main {
     public static void main(String[] args) {
       
-        System.out.println("Hello, i changed this!");
+        System.out.println("Hello, i changed this and added some words!");
 
     }
 }
